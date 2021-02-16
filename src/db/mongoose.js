e62c6f3e3712f0567c067074472526c571455fa0
GREENTECK9810 <vishal.waliyan47@gmail.com>
2021-02-16 11:15:50 +0530

    setting up for heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/chat', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
